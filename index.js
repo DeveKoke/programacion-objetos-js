6db3e7819af290f47504b742b1dce83b3b5d41c5
DeveKoke <jorge.be86@gmail.com>
2023-05-02 22:06:19 +0200

    soluciones

---
+++ index.js
@@ -0,0 +1,106 @@
+// // * 1
+// const coche = {marca: 'peugeot', modelo: 307, matricula: '2033pwd'};
+
+// // * 2
+// const casa = {codPostal: 28031, calle: 'estrada', portal: 3, piso: 4}
+
+// // * 3
+// FullStackDeveloper = {arrayLenguajes: true, arrayProyectos: true}
+
+// // * 4
+// const  perro = { nombre: 'cruce', color: 'pardo' , edad: 6, 'funcion ladrar': 'guau!, guau!', 'funcion popo':Math.random()*3 };
+// let bark = perro['funcion ladrar'];
+// let popo = perro['funcion popo'];
+// console.log(bark);
+// console.log(popo);
+
+// // *5
+// let portatil = {marca: 'Asus'};
+// const marcaPortatil = portatil.marca
+
+// // * 6 
+// let portatil = {'marca': 'HP'};
+// let marca = potatil['marca'];
+// console.log(marca);
+
+// // * 7
+// let concierto = {'array grupos': 6};
+// let grupos = concierto['array grupos'];
+
+
+// // * 8 
+// let led  = {rojo, verde, azul};
+// let arrayRGB = [led.rojo, led.verde, led.azul];
+
+// // * 9
+// let portatil = {modelo: 'VivoBook'};
+// portatil.modelo = 'P345';
+
+// // * 10
+// let concierto = {cartelera: "Guns N' roses"};
+
+// // * 11
+// let concierto = {fecha};
+// concierto.fecha = new Date('Apr 29 2023');
+
+// // * 12
+// let impresora = {imprimiendo};
+// impresora.imprimiendo = {nombreArchivo, copias, numPaginas};
+
+// // * 13
+// let noticia = {titular, cuerpo};
+
+
+// // * 14
+// let persona = {nombre, apellidos, edad}
+
+// * 15
+let avion = {numPasajeros: 504, 'funcion despegar': 'despegando', 'funcion volar': 'llegando al destino', 'funcion aterrizar': 'aterrizando' };
+console.log(['funcion despegar']);
+console.log(['funcion volar']);
+console.log(['funcion aterrizar']);
+
+// *16
+let paquete = {array: [pañuelo, libro, maceta]}
+
+// * 17
+let pais = {numHabitantes:, continente:, gentilicio:};
+
+
+// * 18
+let 0_Error = {codigo: 404};
+let codError = 0_Error.codigo;
+
+// * 19
+let grupo = {'array integrantes': ['jorge', 'Raúl', 'Belén']};
+let integrantes = grupo['array integrantes'];
+
+
+// * 20
+let impresora = {'objeto tinta': {rojo, verde, azul}};
+let nivelesTinta = impresora['objeto tinta'];
+// *21
+let pantalla = {'array bidimensional pixeles': [[1, 2][3, 4]]};
+let pixeles = pantalla['array bidimensional pixeles'];
+// * 22
+let movil = {'especificaciones'};
+let especificaciones = movil['especificaciones'];
+// * 23
+let grupo = {numIntegrantes: 0};
+grupo.numIntegrantes = 5;
+// * 24
+let pantalla = {dimensiones: 130*56};
+pantalla.dimensiones = 1920*1080;
+// * 25
+let led = {encendido};
+if (led.encendido = true){
+    led.encendido = false;
+}else if (led.encendido = false){
+    led.encendido = true;
+}
+
+// *26
+let movil = {temperatura};
+movil.temperatura = '20º';
+
+

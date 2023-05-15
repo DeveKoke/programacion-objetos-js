d613ed5f3843b3a05dc6c10b6127f9164890ffd6
DeveKoke <jorge.be86@gmail.com>
2023-05-15 21:24:14 +0200

    finish

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-// // * 1
-// const coche = {marca: 'peugeot', modelo: 307, matricula: '2033pwd'};
+// * 1
+const coche = {marca: 'peugeot', modelo: 307, matricula: '2033pwd'};
 
-// // * 2
-// const casa = {codPostal: 28031, calle: 'estrada', portal: 3, piso: 4}
+// * 2
+const casa = {codPostal: 28031, calle: 'estrada', portal: 3, piso: 4}
 
-// // * 3
-// FullStackDeveloper = {arrayLenguajes: true, arrayProyectos: true}
+// * 3
+FullStackDeveloper = {arrayLenguajes: true, arrayProyectos: true}
 
-// // * 4
-// const  perro = { nombre: 'cruce', color: 'pardo' , edad: 6, 'funcion ladrar': 'guau!, guau!', 'funcion popo':Math.random()*3 };
-// let bark = perro['funcion ladrar'];
-// let popo = perro['funcion popo'];
-// console.log(bark);
-// console.log(popo);
+// * 4
+const  perro = { nombre: 'cruce', color: 'pardo' , edad: 6, 'funcion ladrar': 'guau!, guau!', 'funcion popo':Math.random()*3 };
+let bark = perro['funcion ladrar'];
+let popo = perro['funcion popo'];
+console.log(bark);
+console.log(popo);
 
-// // *5
-// let portatil = {marca: 'Asus'};
-// const marcaPortatil = portatil.marca
+// *5
+let portatil = {marca: 'Asus'};
+const marcaPortatil = portatil.marca
 
-// // * 6 
-// let portatil = {'marca': 'HP'};
-// let marca = potatil['marca'];
-// console.log(marca);
+// * 6 
+let portatil = {'marca': 'HP'};
+let marca = potatil['marca'];
+console.log(marca);
 
-// // * 7
-// let concierto = {'array grupos': 6};
-// let grupos = concierto['array grupos'];
-
-
-// // * 8 
-// let led  = {rojo, verde, azul};
-// let arrayRGB = [led.rojo, led.verde, led.azul];
-
-// // * 9
-// let portatil = {modelo: 'VivoBook'};
-// portatil.modelo = 'P345';
-
-// // * 10
-// let concierto = {cartelera: "Guns N' roses"};
-
-// // * 11
-// let concierto = {fecha};
-// concierto.fecha = new Date('Apr 29 2023');
-
-// // * 12
-// let impresora = {imprimiendo};
-// impresora.imprimiendo = {nombreArchivo, copias, numPaginas};
-
-// // * 13
-// let noticia = {titular, cuerpo};
+// * 7
+let concierto = {'array grupos': 6};
+let grupos = concierto['array grupos'];
 
 
-// // * 14
-// let persona = {nombre, apellidos, edad}
+// * 8 
+let led  = {rojo, verde, azul};
+let arrayRGB = [led.rojo, led.verde, led.azul];
+
+// * 9
+let portatil = {modelo: 'VivoBook'};
+portatil.modelo = 'P345';
+
+// * 10
+let concierto = {cartelera: "Guns N' roses"};
+
+// * 11
+let concierto = {fecha};
+concierto.fecha = new Date('Apr 29 2023');
+
+// * 12
+let impresora = {imprimiendo};
+impresora.imprimiendo = {nombreArchivo, copias, numPaginas};
+
+// * 13
+let noticia = {titular, cuerpo};
+
+
+// * 14
+let persona = {nombre, apellidos, edad}
 
 // * 15
 let avion = {numPasajeros: 504, 'funcion despegar': 'despegando', 'funcion volar': 'llegando al destino', 'funcion aterrizar': 'aterrizando' };
